ca0733a5ba250c3b378a8c22a031fca2a922e299
Joseph Rawson <joseph.rawson.works@gmail.com>
2013-01-08 06:41:06 -0600

    add code to populate database

--- phoebe/__init__.py
+++ phoebe/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from .models import (
     DBSession,
     Base,
+    MyModel,
     )
 
 def main(global_config, **settings):
@@ -12,6 +13,13 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+    if True:
+        import transaction
+        Base.metadata.create_all(engine)
+        with transaction.manager:
+            model = MyModel(name='one', value=1)
+            DBSession.add(model)
+            
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
