ce8016e0e7fa4398b32c3e9b6fd1f85323d72ec2
Joseph Rawson <joseph.rawson.works@gmail.com>
2013-01-08 08:12:12 -0600

    start using trumpet

--- phoebe/views.py
+++ phoebe/views.py
@@ -7,7 +7,29 @@ from .models import (
     MyModel,
     )
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+from trumpet.views.base import BaseViewer
+from trumpet.views.base import prepare_layout
+
+def prepare_main_layout(request):
+    layout = request.layout_manager.layout
+    prepare_layout(layout)
+    layout.left_menu.set_header('Leaflet Menu')
+    #url = request.route_url('hubby_context', context='dbmeetings', id=None)
+    #layout.left_menu.append_new_entry('hubby', url)
+    url = request.route_url('view_wiki')
+    layout.left_menu.append_new_entry('wiki', url)
+    url = request.route_url('rssviewer', context='listfeeds', feed=None)
+    layout.left_menu.append_new_entry('rss', url)
+    layout.title = 'Leaflet'
+    layout.header = 'Leaflet'
+    layout.subheader = 'Revealed from opening a small mailbox.'
+    
+class MainViewer(BaseViewer):
+    def __init__(self, request):
+        super(MainViewer, self).__init__(request)
+        prepare_main_layout(self.request)
+        
+        
 def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
